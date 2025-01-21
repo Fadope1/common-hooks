705745c3b422aa6fcf242e7d2c3371ca717732c6
peschkfa <fabian.peschke@basf.com>
2025-01-21 18:14:52 +0100

    improved exception condition class

--- common_hooks/conditions/exception_condition.py
+++ common_hooks/conditions/exception_condition.py
@@ -1,5 +1,7 @@
 """Implementation of a exception condition."""
 
+from collections.abc import Container
+
 from .condition import Condition
 
 
@@ -9,13 +11,16 @@ class ExceptionCondition(Condition):
     def __init__(
         self,
         escaped: bool = False,
+        exceptions: Container[Exception] | None = None,
     ) -> None:
         """Initialize the condition.
 
         Args:
             escaped (bool, optional): Whether to only trigger for non-escaped exceptions
+            exceptions (Container[Exception], optional): Exceptions to trigger the condition for
         """
         self.escaped = escaped
+        self.exceptions: Container[Exception] = exceptions or set()
 
     def matches(self, escaped: bool) -> bool:
         """Check if the conditions are met.
